125a1375e41d01dcc74ca0e88fcab3c4b5a1607b
Kea Kohv <55859977+KeaKohv@users.noreply.github.com>
2023-05-23 11:28:35 +0300

    lightgcn functions

---
+++ lightgcn.py
@@ -0,0 +1,171 @@
+import torch
+from torch import Tensor
+import random
+
+import os
+os.environ['TORCH'] = torch.__version__
+
+from torch import nn, optim
+from torch_sparse import SparseTensor, matmul
+from torch_geometric.utils import structured_negative_sampling
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.typing import Adj
+
+from metrics import get_metrics
+
+
+# function which random samples a mini-batch of positive and negative samples
+def sample_mini_batch(batch_size, edge_index):
+    """Randomly samples indices of a minibatch given an adjacency matrix
+
+    Args:
+        batch_size (int): minibatch size
+        edge_index (torch.Tensor): 2 by N list of edges
+
+    Returns:
+        tuple: user indices, positive item indices, negative item indices
+    """
+    edges = structured_negative_sampling(edge_index)
+    edges = torch.stack(edges, dim=0)
+    indices = random.choices(
+        [i for i in range(edges[0].shape[0])], k=batch_size)
+    batch = edges[:, indices]
+    user_indices, pos_item_indices, neg_item_indices = batch[0], batch[1], batch[2]
+    return user_indices, pos_item_indices, neg_item_indices
+
+
+# defines LightGCN model
+class LightGCN(MessagePassing):
+    """LightGCN Model as proposed in https://arxiv.org/abs/2002.02126
+    """
+
+    def __init__(self, num_users, num_items, embedding_dim=64, K=3, add_self_loops=False):
+        """Initializes LightGCN Model
+
+        Args:
+            num_users (int): Number of users
+            num_items (int): Number of items
+            embedding_dim (int, optional): Dimensionality of embeddings. Defaults to 8.
+            K (int, optional): Number of message passing layers. Defaults to 3.
+            add_self_loops (bool, optional): Whether to add self loops for message passing. Defaults to False.
+        """
+        super().__init__()
+        self.num_users, self.num_items = num_users, num_items
+        self.embedding_dim, self.K = embedding_dim, K
+        self.add_self_loops = add_self_loops
+
+        self.users_emb = nn.Embedding(
+            num_embeddings=self.num_users, embedding_dim=self.embedding_dim) # e_u^0
+        self.items_emb = nn.Embedding(
+            num_embeddings=self.num_items, embedding_dim=self.embedding_dim) # e_i^0
+
+        nn.init.normal_(self.users_emb.weight, std=0.1)
+        nn.init.normal_(self.items_emb.weight, std=0.1)
+
+    def forward(self, edge_index: SparseTensor):
+        """Forward propagation of LightGCN Model.
+
+        Args:
+            edge_index (SparseTensor): adjacency matrix
+
+        Returns:
+            tuple (Tensor): e_u_k, e_u_0, e_i_k, e_i_0
+        """
+        # compute \tilde{A}: symmetrically normalized adjacency matrix
+        edge_index_norm = gcn_norm(
+            edge_index, add_self_loops=self.add_self_loops)
+
+        emb_0 = torch.cat([self.users_emb.weight, self.items_emb.weight]) # E^0
+        embs = [emb_0]
+        emb_k = emb_0
+
+        # multi-scale diffusion
+        for i in range(self.K):
+            emb_k = self.propagate(edge_index_norm, x=emb_k)
+            embs.append(emb_k)
+
+        embs = torch.stack(embs, dim=1)
+        emb_final = torch.mean(embs, dim=1) # E^K
+
+        users_emb_final, items_emb_final = torch.split(
+            emb_final, [self.num_users, self.num_items]) # splits into e_u^K and e_i^K
+
+        # returns e_u^K, e_u^0, e_i^K, e_i^0
+        return users_emb_final, self.users_emb.weight, items_emb_final, self.items_emb.weight
+
+    def message(self, x_j: Tensor) -> Tensor:
+        return x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
+        # computes \tilde{A} @ x
+        return matmul(adj_t, x)
+
+
+# Bayesian Personalized Ranking (BPR) loss is a pairwise objective which encourages the predictions of positive samples to be higher than negative samples for each user.
+def bpr_loss(users_emb_final, users_emb_0, pos_items_emb_final, pos_items_emb_0, neg_items_emb_final, neg_items_emb_0, lambda_val):
+    """Bayesian Personalized Ranking Loss as described in https://arxiv.org/abs/1205.2618
+
+    Args:
+        users_emb_final (torch.Tensor): e_u_k
+        users_emb_0 (torch.Tensor): e_u_0
+        pos_items_emb_final (torch.Tensor): positive e_i_k
+        pos_items_emb_0 (torch.Tensor): positive e_i_0
+        neg_items_emb_final (torch.Tensor): negative e_i_k
+        neg_items_emb_0 (torch.Tensor): negative e_i_0
+        lambda_val (float): lambda value for regularization loss term
+
+    Returns:
+        torch.Tensor: scalar bpr loss value
+    """
+    reg_loss = lambda_val * (users_emb_0.norm(2).pow(2) +
+                             pos_items_emb_0.norm(2).pow(2) +
+                             neg_items_emb_0.norm(2).pow(2)) # L2 loss
+
+    pos_scores = torch.mul(users_emb_final, pos_items_emb_final)
+    pos_scores = torch.sum(pos_scores, dim=-1) # predicted scores of positive samples
+    neg_scores = torch.mul(users_emb_final, neg_items_emb_final)
+    neg_scores = torch.sum(neg_scores, dim=-1) # predicted scores of negative samples
+
+    loss = -torch.mean(torch.nn.functional.softplus(pos_scores - neg_scores)) + reg_loss
+
+    return loss
+
+
+# wrapper function to evaluate model
+def evaluation(model, edge_index, sparse_edge_index, exclude_edge_indices, K, lambda_val):
+    """Evaluates model loss and metrics including recall, precision, ndcg @ k
+
+    Args:
+        model (LighGCN): lightgcn model
+        edge_index (torch.Tensor): 2 by N list of edges for split to evaluate
+        sparse_edge_index (sparseTensor): sparse adjacency matrix for split to evaluate
+        exclude_edge_indices ([type]): 2 by N list of edges for split to discount from evaluation
+        k (int): determines the top k items to compute metrics on
+        lambda_val (float): determines lambda for bpr loss
+
+    Returns:
+        tuple: bpr loss, dictionary recall @ k, dictionary precision @ k, dictionary ndcg @ k
+    """
+    # get embeddings
+    users_emb_final, users_emb_0, items_emb_final, items_emb_0 = model.forward(
+        sparse_edge_index)
+    edges = structured_negative_sampling(
+        edge_index, contains_neg_self_loops=False)
+    user_indices, pos_item_indices, neg_item_indices = edges[0], edges[1], edges[2]
+    users_emb_final, users_emb_0 = users_emb_final[user_indices], users_emb_0[user_indices]
+    pos_items_emb_final, pos_items_emb_0 = items_emb_final[
+        pos_item_indices], items_emb_0[pos_item_indices]
+    neg_items_emb_final, neg_items_emb_0 = items_emb_final[
+        neg_item_indices], items_emb_0[neg_item_indices]
+
+    loss = bpr_loss(users_emb_final, users_emb_0, pos_items_emb_final, pos_items_emb_0,
+                    neg_items_emb_final, neg_items_emb_0, lambda_val).item()
+    
+    print('Loss: ', loss)
+
+    recall, precision, ndcg = get_metrics(
+        model, edge_index, exclude_edge_indices, K)
+
+    return loss, recall, precision, ndcg
+
